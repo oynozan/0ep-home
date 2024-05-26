b454e1755190ad7f69450e34c81904db951a0d6d
oynozan <oynozan@hotmail.com>
2024-05-26 17:05:17 +0300

    responsiveness fixes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,9 @@ import './footer.scss';
 export default function Footer() {
     return (
         <footer>
-            0ep Protocol zkChat &copy; - 2024 - Built by&nbsp;<a href="https://oynozan.com/">oynozan</a>
+            <p>
+                0ep Protocol zkChat &copy; - 2024 - Built by&nbsp;<a href="https://oynozan.com/">oynozan</a>
+            </p>
         </footer>
     )
 }
